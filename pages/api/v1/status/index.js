d8ba9092c921e5541bbc9ffed748ae3f331174de
Blzvini <lopes.viniciusm99@gmail.com>
2026-02-27 00:47:35 -0300

    Criando o arquivo de config do database.js e o .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,13 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("Select 1 + 1 AS SUM;");
+  console.log(result.rows);
+
   res
     .status(200)
     .json({ message: "Endpoint de Status sendo acessado com sucessoô" });
+  console.log(database);
 }
 
 export default status;
