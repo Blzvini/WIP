6155369f022c712034d7f75130bdce6fca9e7662
Blzvini <lopes.viniciusm99@gmail.com>
2026-03-02 14:13:52 -0300

    Finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,33 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("Select 1 + 1 AS SUM;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  res
-    .status(200)
-    .json({ message: "Endpoint de Status sendo acessado com sucessoô" });
+  const databaseVersion = await database.query("SHOW server_version;");
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsResult = parseInt(
+    databaseMaxConnections.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const currentConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnections = currentConnections.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version_postgres: databaseVersion.rows[0],
+        max_connections: maxConnectionsResult,
+        current_connections: databaseOpenedConnections,
+      },
+    },
+  });
   console.log(database);
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version_postgres).toEqual({
+    server_version: "16.12",
+  });
+
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  expect(responseBody.dependencies.database.current_connections).toBe(1);
 });
